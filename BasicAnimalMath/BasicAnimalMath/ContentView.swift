c8363dc1e5b39b6d64b4033156193c88ec1729b3
Eric <ejohnson1028@gmail.com>
2023-02-27 21:39:44 -0800

    Basic app layout
    Got some basic app layout set and assets in place

--- BasicAnimalMath/BasicAnimalMath/ContentView.swift
+++ BasicAnimalMath/BasicAnimalMath/ContentView.swift
@@ -7,15 +7,86 @@
 
 import SwiftUI
 
+struct AdditionProblems {
+    
+}
+
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            ZStack {
+                Color.green.ignoresSafeArea()
+                VStack {
+                    
+                    Spacer()
+                    
+                    VStack {
+                        Section {
+                            HStack {
+                                ForEach(0..<4, id: \.self) { num in
+                                    Image("giraffe")
+                                        .resizable()
+                                        .frame(width: 50, height: 50)
+                                        .scaledToFit()
+                                }
+                            }
+                            
+                            Text("4")
+                                .font(.system(size: 40))
+                                .foregroundColor(.primary)
+                                .padding()
+                            Image(systemName: "plus")
+                                .font(.system(size: 50))
+                                .foregroundColor(.white)
+                                .padding()
+                            Text("2")
+                                .font(.system(size: 40))
+                                .foregroundColor(.primary)
+                                .padding()
+                            
+                            HStack {
+                                ForEach(0..<2, id: \.self) { num in
+                                    Image("giraffe")
+                                        .resizable()
+                                        .frame(width: 50, height: 50)
+                                        .scaledToFit()
+                                }
+                            }
+                        }
+                    } // Problem Window
+                    .frame(width: 300, height: 400)
+                    .background(.ultraThinMaterial)
+                    .clipShape(RoundedRectangle(cornerRadius: 25))
+                    
+                    Spacer()
+                    
+                    VStack {
+                        Section {
+                            ForEach(0..<4, id:\.self) { num in
+                                Button("\(num)") {
+                                    //Do Something
+                                }
+                                .foregroundColor(.primary)
+                            }
+                        }
+                        .frame(width: 250, height: 50)
+                        .background(.thinMaterial)
+                        .clipShape(RoundedRectangle(cornerRadius: 25))
+                    } // Answer Picker Window
+                    
+                    Spacer()
+                }
+            }
+            .navigationTitle("🦁 Basic Animal Math 🐮")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                Button {
+                    // Code to come
+                } label: {
+                    Image(systemName: "gearshape")
+                }
+            }
         }
-        .padding()
     }
 }
 

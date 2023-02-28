c289c1c90e95fadabcdbef76ee273759c8ac903d
Eric <ejohnson1028@gmail.com>
2023-02-27 19:52:53 -0800

    Init Commit
    Created BasicAnimal Math app and repository

---
+++ BasicAnimalMath/BasicAnimalMath/BasicAnimalMathApp.swift
@@ -0,0 +1,17 @@
+//
+//  BasicAnimalMathApp.swift
+//  BasicAnimalMath
+//
+//  Created by Eric Johnson on 2/27/23.
+//
+
+import SwiftUI
+
+@main
+struct BasicAnimalMathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
